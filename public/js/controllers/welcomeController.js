1a1b59b9cbb37fd23093a96679eea055d07f5ecd
Jeffrey Macks <jmacks1988@gmail.com>
2016-02-16 18:55:25 -0500

    can now retrieve yelp info from the backend and render it in the angular views

--- public/js/controllers/welcomeController.js
+++ public/js/controllers/welcomeController.js
@@ -1,14 +1,19 @@
 'use strict';
 (function(){
-
+  var randomNum = Math.floor(Math.random() * 19) + 1
+  console.log(randomNum);
   var welcomeController = function($scope, $log, foodFactory){
+
       function initFood(){
         foodFactory.getYelpAPI()
                 .success(function(food){
-                  $scope.food = angular.fromJson(food.businesses[0]);
-                  $scope.rating = angular.fromJson(food.businesses[0].rating);
+                  $scope.restaurant = food.businesses[randomNum];
+                  $scope.rating = food.businesses[randomNum].rating;
+                  $scope.name = food.businesses[randomNum].name;
+                  $scope.reviewSnippet = food.businesses[randomNum].snippet_text;
+                  $scope.phone = food.businesses[randomNum].phone;
+                  $scope.crossStreets = food.businesses[randomNum].location.cross_streets;
 
-                  console.log($scope.rating);
                 })
                 .error(function(data, status){
                   $log.log('theres an error');
